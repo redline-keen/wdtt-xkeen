0b4984259f6e4680cce460c504e8cd3486fb2131
redline-keen <311057708+redline-keen@users.noreply.github.com>
2026-09-13 22:19:26 +0300

    Update wdtt-xkeen-install-terminal-13092026-1245-vktoken-rotate.sh

--- wdtt-xkeen-install-terminal-13092026-1245-vktoken-rotate.sh
+++ wdtt-xkeen-install-terminal-13092026-1245-vktoken-rotate.sh
@@ -38,7 +38,7 @@ run_installer() {
 
     GH_OWNER="redline-keen"
     GH_REPO="wdtt-xkeen"
-    GH_TAG="1.3"
+    GH_TAG="1.2"
 
     ASSET_MIPSLE="wdtt-client-mipsle"
     ASSET_ARM64="wdtt-client-arm64"
